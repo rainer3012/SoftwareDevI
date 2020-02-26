0ab6017427b11652e2214eb4589bd48c2f5f32e7
rainer3012 <59969495+rainer3012@users.noreply.github.com>
2020-02-26 10:21:45 -0500

    Cleaned up and commented code

--- SDMod5/src/Fibonacci.java
+++ SDMod5/src/Fibonacci.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Fibonacci {
 
+	// Recursive Function
 	public static int fibRecursive(int nFib) {
 		if (nFib == 0) {
 			return 0;
@@ -12,6 +12,7 @@ public class Fibonacci {
 		return fibRecursive(nFib - 1) + fibRecursive(nFib - 2);
 	}
 
+	// Iterative Function
 	public static int fibIterative(int nFib) {
 		int oldOldNumber;
 		int oldNumber = 0;
@@ -21,22 +22,22 @@ public class Fibonacci {
 			return 0;
 		} else {
 			for (int i = 0; i < nFib; i++) {
-
 				oldOldNumber = oldNumber;
 				oldNumber = currNumber;
 				currNumber = oldOldNumber + oldNumber;
-
 			}
 			return currNumber;
 		}
 	}
 
+	// Recursive Timing Loop
 	public static double timedRecursive(int nFib) {
 		double timeStart = System.nanoTime();
 		System.out.println(fibRecursive(nFib));
 		return (System.nanoTime() - timeStart);
 	}
 
+	// Iterative Timing Loop
 	public static double timedIterative(int nFib) {
 		double timeStart = System.nanoTime();
 		System.out.println(fibIterative(nFib));
@@ -45,24 +46,22 @@ public class Fibonacci {
 
 	public static void main(String[] args) {
 
-		ArrayList<Double> iterativeTimes = new ArrayList<Double>();
-		ArrayList<Double> recursiveTimes = new ArrayList<Double>();
+		ArrayList<Double> iterativeTimes = new ArrayList<Double>(); //ArrayList to store iterative results
+		ArrayList<Double> recursiveTimes = new ArrayList<Double>(); //ArrayList to store recursive results
 
-		Scanner input = new Scanner(System.in);
-
+		// iterative loop
 		System.out.println("Iterative tests:");
 		for (int i = 0; i < 30; i++) {
 			iterativeTimes.add(timedIterative(i));
 			System.out.println(iterativeTimes.get(i));
 		}
 
+		// recursive loop
 		System.out.println("Recursive tests:");
-		for (int i = 0; i < 31; i++) {
+		for (int i = 0; i < 31; i++) {	          // the recursive loop is calculated an extra time due to 0 and 1 breaking the function
 			recursiveTimes.add(timedRecursive(i));
 			System.out.println(recursiveTimes.get(i));
 		}
-
-		input.close();
 
 	}
 
